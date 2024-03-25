a72c381b0c314f9ccad43029d71a7820c8bf7e01
bearfm <melchi@garfias.org>
2024-03-24 21:39:19 -0700

    Automated Commit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -97,7 +97,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.bear = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "render" "video" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "render" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
     ];
