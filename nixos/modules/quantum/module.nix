9c5a86549aff18f3a17f074f4f62076b52080024
bearfm <77757734+bearfm@users.noreply.github.com>
2024-05-26 14:00:55 -0700

    Automated Commit

--- nixos/modules/quantum/module.nix
+++ nixos/modules/quantum/module.nix
@@ -17,6 +17,7 @@
     ];
 
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
