eea389bfdf422c7b35bff82812705f03caeaa12e
bearfm <77757734+bearfm@users.noreply.github.com>
2024-06-08 22:12:18 -0700

    Automated Commit

--- nixos/modules/quantum/wm.nix
+++ nixos/modules/quantum/wm.nix
@@ -5,35 +5,35 @@
 				enable = true;
 		};
 
-		environment.systemPackages = with pkgs; [
-			wayfire-with-plugins
-		];
+		#environment.systemPackages = with pkgs; [
+		#	wayfire-with-plugins
+		#];
 
-		services.greetd = {
-			enable = true;
-			settings = {
-				default_session = {
-					command = "${pkgs.greetd.greetd}/bin/agreety --cmd wayfire";
-					user = "bear";
-				};
-			};
-		};
-
-		#services.displayManager.sddm = {
+		#services.greetd = {
 		#	enable = true;
-		#	wayland = {
-		#		enable = true;
+		#	settings = {
+		#		default_session = {
+		#			command = "${pkgs.greetd.greetd}/bin/agreety --cmd wayfire";
+		#			user = "bear";
+		#		};
 		#	};
 		#};
 
-		#services.desktopManager = {
-		#	plasma6.enable = true;
-		#};
-#
-#		environment.plasma6.excludePackages = with pkgs.kdePackages; [
-#			konsole
-#		];
-#		
+		services.displayManager.sddm = {
+			enable = true;
+			wayland = {
+				enable = true;
+			};
+		};
+
+		services.desktopManager = {
+			plasma6.enable = true;
+		};
+
+		environment.plasma6.excludePackages = with pkgs.kdePackages; [
+			konsole
+		];
+
 		systemd.targets.sleep.enable = false;
 		systemd.targets.suspend.enable = false;
 		systemd.targets.hibernate.enable = false;
