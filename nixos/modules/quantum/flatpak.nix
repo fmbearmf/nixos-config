6e62293a58bc9e7e4d420271034acc50bca85f6f
bearfm <77757734+bearfm@users.noreply.github.com>
2024-06-09 21:27:30 -0700

    Automated Commit

--- nixos/modules/quantum/flatpak.nix
+++ nixos/modules/quantum/flatpak.nix
@@ -4,8 +4,7 @@
 	services.flatpak.enable = true;
 	xdg.portal = {
 		enable = true;
-		wlr.enable = true;
 		extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-		config.common.default = "wlr";
+		config.common.default = "gtk";
 	};
 }
