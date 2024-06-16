f61eb62fa970b6f4bc51272cbc81bb17f6331367
bearfm <77757734+bearfm@users.noreply.github.com>
2024-06-16 16:52:28 -0700

    Automated Commit

--- nixos/modules/quantum/nvim/default.nix
+++ nixos/modules/quantum/nvim/default.nix
@@ -3,5 +3,27 @@
 
 	programs.nixvim = {
 		enable = true;
+
+		colorschemes.gruvbox.enable = true;
+
+		plugins = {
+			lualine.enable = true;
+			lsp = {
+				enable = true;
+				servers = {
+					tsserver.enable = true;
+					lua-ls.enable = true;
+					rust-analyzer = {
+						enable = true;
+						installCargo = false;
+						installRustc = false;
+					};
+				};
+			};
+			cmp = {
+				enable = true;
+				autoEnableSources = true;
+			};
+		};
 	};
 }
