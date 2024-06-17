9d79ef31150d6562cc93a17eb82e0089924a67d1
bearfm <77757734+bearfm@users.noreply.github.com>
2024-06-16 17:17:32 -0700

    Automated Commit

--- nixos/modules/quantum/nvim/default.nix
+++ nixos/modules/quantum/nvim/default.nix
@@ -28,6 +28,34 @@
 			oil.enable = true;
 			treesitter.enable = true;
 			luasnip.enable = true;
+			indent-blankline = {
+				enable = true;
+				settings = {
+					indent = {
+						char = "│"; # "│" or "▎"
+					};
+					scope = {
+						enabled = true;
+						show_start = true;
+					};
+					exclude = {
+						buftypes = ["terminal" "nofile"];
+						filetypes = [
+							"help"
+							"alpha"
+							"dashboard"
+							"neo-tree"
+							"Trouble"
+							"trouble"
+							"lazy"
+							"mason"
+							"notify"
+							"toggleterm"
+							"lazyterm"
+						];
+					};
+				};
+			};
 		};
 	};
 }
