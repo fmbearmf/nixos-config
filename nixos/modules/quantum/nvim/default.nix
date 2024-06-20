a6e28c5765d989ab1bbdc446749670d11604a6ec
bearfm <77757734+bearfm@users.noreply.github.com>
2024-06-19 20:13:36 -0700

    Automated Commit

--- nixos/modules/quantum/nvim/default.nix
+++ nixos/modules/quantum/nvim/default.nix
@@ -4,6 +4,7 @@
 		./autoclose.nix
 		./neotree.nix
 		./keymaps.nix
+		./plugin-monolith.nix
 	];
 
 	programs.nixvim = {
@@ -12,57 +13,5 @@
 		vimAlias = true;
 
 		colorschemes.gruvbox.enable = true;
-
-		plugins = {
-			lualine.enable = true;
-			lsp = {
-				enable = true;
-				servers = {
-					tsserver.enable = true;
-					lua-ls.enable = true;
-					rust-analyzer = {
-						enable = true;
-						installCargo = false;
-						installRustc = false;
-					};
-				};
-			};
-			cmp = {
-				enable = true;
-				autoEnableSources = true;
-			};
-			telescope.enable = true;
-			oil.enable = true;
-			treesitter.enable = true;
-			luasnip.enable = true;
-			indent-blankline = {
-				enable = true;
-				settings = {
-					indent = {
-						char = "│"; # "│" or "▎"
-					};
-					scope = {
-						enabled = true;
-						show_start = true;
-					};
-					exclude = {
-						buftypes = ["terminal" "nofile"];
-						filetypes = [
-							"help"
-							"alpha"
-							"dashboard"
-							"neo-tree"
-							"Trouble"
-							"trouble"
-							"lazy"
-							"mason"
-							"notify"
-							"toggleterm"
-							"lazyterm"
-						];
-					};
-				};
-			};
-		};
 	};
 }
---
+++ nixos/modules/quantum/nvim/plugin-monolith.nix
@@ -0,0 +1,60 @@
+{
+  # the behemoth of un-refactored plugin configs
+  programs.nixvim.plugins = {
+    lualine.enable = true;
+    lsp = {
+      enable = true;
+      servers = {
+        tsserver.enable = true;
+        lua-ls.enable = true;
+        rust-analyzer = {
+          enable = true;
+          installCargo = false;
+          installRustc = false;
+        };
+      };
+    };
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+    };
+    telescope.enable = true;
+    oil.enable = true;
+    treesitter.enable = true;
+    luasnip.enable = true;
+    indent-blankline = {
+      enable = true;
+      settings = {
+        indent = {
+          char = "│"; # "│" or "▎"
+        };
+        scope = {
+          enabled = true;
+          show_start = true;
+        };
+        exclude = {
+          buftypes = ["terminal" "nofile"];
+          filetypes = [
+            "help"
+            "alpha"
+            "dashboard"
+            "neo-tree"
+            "Trouble"
+            "trouble"
+            "lazy"
+            "mason"
+            "notify"
+            "toggleterm"
+            "lazyterm"
+          ];
+        };
+      };
+    };
+    toggleterm = {
+      enable = true;
+      settings = {
+        open_mapping = "[[<C-t>]]";
+      };
+    };
+  };
+}
