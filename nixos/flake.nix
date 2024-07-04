4018e5698e1343407cf61bf0b3bdc46607c0dfa3
bearfm <77757734+bearfm@users.noreply.github.com>
2024-07-04 04:09:24 -0700

    Automated Commit

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -11,14 +11,13 @@
 			url = "github:nix-community/nixvim";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
-		chaotic = {
-			url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-		};
+		chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+		alejandra.url = "github:kamadorueda/alejandra/3.0.0";
 	};
 
-	outputs = { self, nixpkgs, chaotic, fenix, nixvim, ... }@inputs: {
+	outputs = { self, nixpkgs, chaotic, fenix, nixvim, alejandra, ... }@inputs: {
 		packages.x86_64-linux.default = fenix.packages.x86_64-linux.minimal.toolchain;
-		nixosConfigurations.quantum = nixpkgs.lib.nixosSystem {
+		nixosConfigurations.quantum = nixpkgs.lib.nixosSystem rec {
 			system = "x86_64-linux";
 			modules = [
 				./modules/quantum/module.nix
@@ -37,6 +36,9 @@
             					rust-analyzer-nightly
           				];
 				})
+				{
+					environment.systemPackages = [ alejandra.defaultPackage.${system} ];
+				}
 			];
 		};
 	};
